2b519c622bc9b3f8fdb7c0c910fd794ee1499437
يزن حمزه <yznhamzeh@gmail.com>
2023-08-10 20:52:43 +0300

    اول اصدار
    نبذه بسيطة عني
    وقائمة بشاريعي

---
+++ src/lib/Project.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string;
+	export let url: string;
+	export let img_url: string;
+</script>
+
+<a target="_blank" class="text-right flex gap-3" href={url}>
+	<img class="w-44 aspect-video" src={img_url} alt={title} />
+	<div class="max-w-[300px] md:max-w-md">
+		<h4 class="font-bold">{title}</h4>
+		<p>{description}</p>
+	</div>
+</a>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import "../app.css";
+  </script>
+  
+  <slot />
+
+  <style lang="postcss">
+            :global(html) {
+      background-color: theme(colors.gray.100);
+    color: theme(colors.gray.950);
+
+    }
+    @media (prefers-color-scheme: dark){
+        :global(html) {
+      background-color: theme(colors.gray.950);
+    color: theme(colors.gray.50);
+    }
+    }
+    
+  </style>
