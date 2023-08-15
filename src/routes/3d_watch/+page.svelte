656f0ab84b80a59426bc41f2b3bec3aafc66f7e5
يزن حمزه <yznhamzeh@gmail.com>
2023-08-15 23:40:34 +0300

    add 3d watch

---
+++ src/routes/3d_watch/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+
+	import { T } from '@threlte/core';
+    import { Environment, OrbitControls } from '@threlte/extras'
+	import Watch from './Watch.svelte';
+
+</script>
+<div style="height: 100vh;">
+    <Canvas>
+        <Environment
+        files="watch_env.hdr"
+        isBackground={true}        
+      />
+      <T.PerspectiveCamera
+        makeDefault
+        position={[1, 1, 1]}
+        fov={85}
+      >
+        <OrbitControls
+        
+          enableDamping
+        />
+      </T.PerspectiveCamera>
+      <T.AmbientLight />
+      <Watch></Watch>
+
+    </Canvas>
+    
+</div>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		{ ...threeMinifier(), enforce: 'pre' }
+	],
+	ssr: {
+		noExternal: ['three']
+	}
 };
 
 export default config;
